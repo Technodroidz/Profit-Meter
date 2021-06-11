76f4e63346cc551a28dd6697baaf031aa5e39e2b
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-06-11 19:17:12 +0530

    missing_shopify_permission

--- app/Http/Controllers/ShopifyApp/AuthController.php
+++ app/Http/Controllers/ShopifyApp/AuthController.php
@@ -183,15 +183,13 @@ class AuthController extends Controller
         if(!empty(Auth::User()->shopify_access_token)){
             $user->shopify_access_token = $accessToken;
         }else{
-             $user->shopify_access_token = $user->shopify_access_token;
+            $user->shopify_access_token = $user->shopify_access_token;
         }
 
         $user->shopify_url = $request->shop;
         $user->save();
 
-        pp(Auth::User());
-
-
+        return redirect()->route('home');
     }
 
     public function logout(Request $request)
