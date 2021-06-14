0cd3ebd09f3dde360437be0694f8f7bbd096dcba
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-06-14 20:48:46 +0530

    login with shopify

--- resources/views/business_app/auth_template/login.blade.php
+++ resources/views/business_app/auth_template/login.blade.php
@@ -73,8 +73,11 @@
                                             </div>
                                         @endif
                                         @if(session()->has('success'))
-                                            <div class="alert alert-danger alert-dismissable hiddenError">
+                                            <div class="alert alert-success alert-dismissable fade show">
                                                 {{ session()->get('success') }}
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
                                             </div>
                                         @endif
 
