728b592b437268e65721e2e1d396027222db4d3f
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-06-21 17:17:18 +0530

    add changes of dilip_develop

---
+++ app/Model/EmailTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailTemplate extends Model
+{
+    protected $table = 'email_templetes';
+    protected $guarded = []; 
+}
