ce48d5293ca61be0c7cc2d383bea26e935134661
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-06-21 15:32:23 +0530

    Revert "Merge branch 'main' of https://github.com/Technodroidz/Profit-Meter into dilip_develop"
    This reverts commit 3b3f8e962c18613954cff1d75c3dd01202d84017, reversing
    changes made to 1803c173917b0844d92e23dccf43357924ec5ba9.

--- app/Http/Controllers/ShopifyApp/DashboardController.php
+++ app/Http/Controllers/ShopifyApp/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\ShopifyApp;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Log;
