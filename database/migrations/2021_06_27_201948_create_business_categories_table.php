dc01f9cdc545cd5e7095f224ea3dd7975023550d
DilipTechD <dilip@technodroidz.xyz>
2021-07-01 13:10:23 +0530

    comment migration

--- database/migrations/2021_06_27_201948_create_business_categories_table.php
+++ database/migrations/2021_06_27_201948_create_business_categories_table.php
@@ -15,63 +15,63 @@ class CreateBusinessCategoriesTable extends Migration
     public function up()
     {
 
-        $users = User::get();
-        foreach ($users as $key => $value) {
-            if(!empty($value->database_name)){
-                if (!Schema::connection($value->database_name)->hasTable('business_categories')) {
-                    // Config::set("database.connections.".$value->database_name, [
-                    //     'driver' => 'mysql',
-                    //     'url' => env('DATABASE_URL'),
-                    //     'host' => env('DB_HOST', '127.0.0.1'),
-                    //     'port' => env('DB_PORT', '3306'),
-                    //     'database' =>$value->database_name,
-                    //     'username' => env('DB_USERNAME', 'forge'),
-                    //     'password' => env('DB_PASSWORD', ''),
-                    //     'unix_socket' => env('DB_SOCKET', ''),
-                    //     'charset' => 'utf8mb4',
-                    //     'collation' => 'utf8mb4_unicode_ci',
-                    //     'prefix' => '',
-                    //     'prefix_indexes' => true,
-                    //     'strict' => true,
-                    //     'engine' => null,
-                    //     'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                    //     ]) : []
-                    // ]);
-                    config(["database.connections.".$value->database_name => [
-                        'driver' => 'mysql',
-                        'url' => env('DATABASE_URL'),
-                        'host' => env('DB_HOST', '127.0.0.1'),
-                        'port' => env('DB_PORT', '3306'),
-                        'database' => $value->database_name,
-                        'username' => env('DB_USERNAME', 'forge'),
-                        'password' => env('DB_PASSWORD', ''),
-                        'unix_socket' => env('DB_SOCKET', ''),
-                        'charset' => 'utf8mb4',
-                        'collation' => 'utf8mb4_unicode_ci',
-                        'prefix' => '',
-                        'prefix_indexes' => true,
-                        'strict' => true,
-                        'engine' => null,
-                        'options' => extension_loaded('pdo_mysql') ? array_filter([
-                        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                        ]) : []
-                        ]
-                    ]);
-                    Schema::connection($value->database_name)->create('business_categories', function (Blueprint $table) {
-                        $table->increments('id');
-                        $table->string('category_name')->nullable();
-                        $table->string('slug_name')->nullable();
-                        $table->string('description')->nullable();
-                        $table->integer('status')->default(1);
-                        $table->softDeletes();
-                        $table->timestamps();
-                    });
-                }
+        // $users = User::get();
+        // foreach ($users as $key => $value) {
+        //     if(!empty($value->database_name)){
+        //         if (!Schema::connection($value->database_name)->hasTable('business_categories')) {
+        //             // Config::set("database.connections.".$value->database_name, [
+        //             //     'driver' => 'mysql',
+        //             //     'url' => env('DATABASE_URL'),
+        //             //     'host' => env('DB_HOST', '127.0.0.1'),
+        //             //     'port' => env('DB_PORT', '3306'),
+        //             //     'database' =>$value->database_name,
+        //             //     'username' => env('DB_USERNAME', 'forge'),
+        //             //     'password' => env('DB_PASSWORD', ''),
+        //             //     'unix_socket' => env('DB_SOCKET', ''),
+        //             //     'charset' => 'utf8mb4',
+        //             //     'collation' => 'utf8mb4_unicode_ci',
+        //             //     'prefix' => '',
+        //             //     'prefix_indexes' => true,
+        //             //     'strict' => true,
+        //             //     'engine' => null,
+        //             //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //             //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //             //     ]) : []
+        //             // ]);
+        //             config(["database.connections.".$value->database_name => [
+        //                 'driver' => 'mysql',
+        //                 'url' => env('DATABASE_URL'),
+        //                 'host' => env('DB_HOST', '127.0.0.1'),
+        //                 'port' => env('DB_PORT', '3306'),
+        //                 'database' => $value->database_name,
+        //                 'username' => env('DB_USERNAME', 'forge'),
+        //                 'password' => env('DB_PASSWORD', ''),
+        //                 'unix_socket' => env('DB_SOCKET', ''),
+        //                 'charset' => 'utf8mb4',
+        //                 'collation' => 'utf8mb4_unicode_ci',
+        //                 'prefix' => '',
+        //                 'prefix_indexes' => true,
+        //                 'strict' => true,
+        //                 'engine' => null,
+        //                 'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //                 ]) : []
+        //                 ]
+        //             ]);
+        //             Schema::connection($value->database_name)->create('business_categories', function (Blueprint $table) {
+        //                 $table->increments('id');
+        //                 $table->string('category_name')->nullable();
+        //                 $table->string('slug_name')->nullable();
+        //                 $table->string('description')->nullable();
+        //                 $table->integer('status')->default(1);
+        //                 $table->softDeletes();
+        //                 $table->timestamps();
+        //             });
+        //         }
 
-            }
+        //     }
 
-        }
+        // }
         
         
     }
--- database/migrations/2021_06_28_204829_create_business_custom_costs_table.php
+++ database/migrations/2021_06_28_204829_create_business_custom_costs_table.php
@@ -15,48 +15,48 @@ class CreateBusinessCustomCostsTable extends Migration
     public function up()
     {
     
-        $users = User::get();
-        foreach ($users as $key => $value) {
-            if(!empty($value->database_name)){
-                if (!Schema::connection($value->database_name)->hasTable('business_custom_costs')) {
-                    Config::set("database.connections.".$value->database_name, [
-                        'driver' => 'mysql',
-                        'url' => env('DATABASE_URL'),
-                        'host' => env('DB_HOST', '127.0.0.1'),
-                        'port' => env('DB_PORT', '3306'),
-                        'database' =>$value->database_name,
-                        'username' => env('DB_USERNAME', 'forge'),
-                        'password' => env('DB_PASSWORD', ''),
-                        'unix_socket' => env('DB_SOCKET', ''),
-                        'charset' => 'utf8mb4',
-                        'collation' => 'utf8mb4_unicode_ci',
-                        'prefix' => '',
-                        'prefix_indexes' => true,
-                        'strict' => true,
-                        'engine' => null,
-                        'options' => extension_loaded('pdo_mysql') ? array_filter([
-                        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                        ]) : []
-                    ]);
-                    Schema::connection($value->database_name)->create('business_custom_costs', function (Blueprint $table) {
-                        $table->increments('id');
-                        $table->string('custom_name')->nullable();
-                        $table->string('custom_slug')->nullable();
-                        $table->unsignedInteger('category_id')->nullable();
-                        $table->foreign('category_id')->references('id')->on('business_categories'); 
-                        $table->string('start_date')->nullable();
-                        $table->string('end_date')->nullable();
-                        $table->string('frequency')->nullable();
-                        $table->string('cost')->nullable();
-                        $table->integer('accept_include_marketing')->nullable();
-                        $table->softDeletes();
-                        $table->timestamps();
-             });
-        }
+    //     $users = User::get();
+    //     foreach ($users as $key => $value) {
+    //         if(!empty($value->database_name)){
+    //             if (!Schema::connection($value->database_name)->hasTable('business_custom_costs')) {
+    //                 Config::set("database.connections.".$value->database_name, [
+    //                     'driver' => 'mysql',
+    //                     'url' => env('DATABASE_URL'),
+    //                     'host' => env('DB_HOST', '127.0.0.1'),
+    //                     'port' => env('DB_PORT', '3306'),
+    //                     'database' =>$value->database_name,
+    //                     'username' => env('DB_USERNAME', 'forge'),
+    //                     'password' => env('DB_PASSWORD', ''),
+    //                     'unix_socket' => env('DB_SOCKET', ''),
+    //                     'charset' => 'utf8mb4',
+    //                     'collation' => 'utf8mb4_unicode_ci',
+    //                     'prefix' => '',
+    //                     'prefix_indexes' => true,
+    //                     'strict' => true,
+    //                     'engine' => null,
+    //                     'options' => extension_loaded('pdo_mysql') ? array_filter([
+    //                     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+    //                     ]) : []
+    //                 ]);
+    //                 Schema::connection($value->database_name)->create('business_custom_costs', function (Blueprint $table) {
+    //                     $table->increments('id');
+    //                     $table->string('custom_name')->nullable();
+    //                     $table->string('custom_slug')->nullable();
+    //                     $table->unsignedInteger('category_id')->nullable();
+    //                     $table->foreign('category_id')->references('id')->on('business_categories'); 
+    //                     $table->string('start_date')->nullable();
+    //                     $table->string('end_date')->nullable();
+    //                     $table->string('frequency')->nullable();
+    //                     $table->string('cost')->nullable();
+    //                     $table->integer('accept_include_marketing')->nullable();
+    //                     $table->softDeletes();
+    //                     $table->timestamps();
+    //          });
+    //     }
 
-    }
+    // }
 
-}
+//}
 
 
 }
