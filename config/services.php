2a780c144e683a6b7c08e40baef4415286b4a1b8
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-07-02 17:44:20 +0530

    google and paypal creds

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1/Profit-Meter/login/google/callback'
+        'redirect' => 'https://citrixcrm.club/login/google/callback'
     ],
 
     'snapchat' => [
