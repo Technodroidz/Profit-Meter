404624f3c9e7b1da61d436e660f6d4e761c7346b
sarlesh-technodroidz <83313260+sarlu-coder@users.noreply.github.com>
2021-09-03 12:21:11 +0530

    multitenant db structure

--- public/db/dummy.sql
+++ public/db/dummy.sql
@@ -251,3 +251,261 @@ ALTER TABLE `business_custom_costs`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `shipping_cost_country_rules` (
+  `id` int(11) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `shipping_cost` int(11) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `status` tinyint(4) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shipping_cost_country_rules`
+--
+ALTER TABLE `shipping_cost_country_rules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shipping_cost_country_rules`
+--
+ALTER TABLE `shipping_cost_country_rules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `shipping_cost_settings` (
+  `id` int(11) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shipping_cost_settings`
+--
+ALTER TABLE `shipping_cost_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shipping_cost_settings`
+--
+ALTER TABLE `shipping_cost_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `shopify_products` (
+  `id` int(11) NOT NULL,
+  `product_id` text DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `body_html` text DEFAULT NULL,
+  `vendor` varchar(255) DEFAULT NULL,
+  `product_type` text DEFAULT NULL,
+  `shopify_created_at` text DEFAULT NULL,
+  `handle` text DEFAULT NULL,
+  `shopify_updated_at` text DEFAULT NULL,
+  `shopify_published_at` text DEFAULT NULL,
+  `template_suffix` text DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `published_scope` text DEFAULT NULL,
+  `tags` text DEFAULT NULL,
+  `admin_graphql_api_id` text DEFAULT NULL,
+  `deleted_at` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopify_products`
+--
+ALTER TABLE `shopify_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopify_products`
+--
+ALTER TABLE `shopify_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `shopify_product_variants` (
+  `id` int(11) NOT NULL,
+  `variant_id` varchar(255) DEFAULT NULL,
+  `product_table_id` int(11) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `product_title` varchar(255) DEFAULT NULL,
+  `price` text DEFAULT NULL,
+  `profitrack_handling_cost` varchar(255) DEFAULT NULL,
+  `sku` text DEFAULT NULL,
+  `position` text DEFAULT NULL,
+  `inventory_policy` text DEFAULT NULL,
+  `compare_at_price` text DEFAULT NULL,
+  `fulfillment_service` text DEFAULT NULL,
+  `inventory_management` text DEFAULT NULL,
+  `option1` text DEFAULT NULL,
+  `option2` text DEFAULT NULL,
+  `option3` text DEFAULT NULL,
+  `shopify_created_at` text DEFAULT NULL,
+  `shopify_updated_at` text DEFAULT NULL,
+  `taxable` text DEFAULT NULL,
+  `barcode` text DEFAULT NULL,
+  `grams` text DEFAULT NULL,
+  `image_id` text DEFAULT NULL,
+  `weight` text DEFAULT NULL,
+  `weight_unit` text DEFAULT NULL,
+  `inventory_item_id` text DEFAULT NULL,
+  `inventory_quantity` text DEFAULT NULL,
+  `old_inventory_quantity` text DEFAULT NULL,
+  `requires_shipping` text DEFAULT NULL,
+  `admin_graphql_api_id` text DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopify_product_variants`
+--
+ALTER TABLE `shopify_product_variants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopify_product_variants`
+--
+ALTER TABLE `shopify_product_variants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `shopify_product_variant_costs` (
+  `id` int(11) NOT NULL,
+  `variant_id` varchar(255) NOT NULL,
+  `profitrack_product_cost` varchar(255) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopify_product_variant_costs`
+--
+ALTER TABLE `shopify_product_variant_costs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopify_product_variant_costs`
+--
+ALTER TABLE `shopify_product_variant_costs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `shopify_product_variant_shipping_costs` (
+  `id` int(11) NOT NULL,
+  `variant_id` varchar(255) NOT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `shipping_cost` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopify_product_variant_shipping_costs`
+--
+ALTER TABLE `shopify_product_variant_shipping_costs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopify_product_variant_shipping_costs`
+--
+ALTER TABLE `shopify_product_variant_shipping_costs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `transaction_costs` (
+  `id` int(11) NOT NULL,
+  `payment_gateway` varchar(255) DEFAULT NULL,
+  `percentage_fee` float(8,4) DEFAULT NULL,
+  `fixed_fee` float(10,4) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction_costs`
+--
+ALTER TABLE `transaction_costs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction_costs`
+--
+ALTER TABLE `transaction_costs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
